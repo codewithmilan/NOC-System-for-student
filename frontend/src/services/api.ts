b3986ce9e04d472060c62f4780bf77c34281c5bd
MILAN KUMAR VERMA <milansgnv22@gmail.com>
2026-02-22 02:52:13 +0530

    connect live backend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "ttps://noc-system-for-student.onrender.com/api/api",
 });
 
 API.interceptors.request.use((config) => {
