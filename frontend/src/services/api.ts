5dd388411dad776839f2c7bb14010374367ee3a7
MILAN KUMAR VERMA <milansgnv22@gmail.com>
2026-02-22 03:28:18 +0530

    fix api base url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://noc-system-for-student.onrender.com",
+  baseURL: "https://noc-system-for-student.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
