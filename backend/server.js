6a2ee7b50d161bc4708dea50689a09ce839c60d8
MILAN KUMAR VERMA <milansgnv22@gmail.com>
2026-02-22 03:02:49 +0530

    fix cors for production

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,11 @@ const pdfRoutes = require("./routes/pdfRoutes");
 const app = express();
 
 // ================= MIDDLEWARE =================
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 // static uploads
